3350752a868e9dc6eabccf5d6e0d8953f90e58f6
Sudharsan <sudharsanbalasubramanian1@gmail.com>
2025-07-12 22:47:29 +0530

    Next greater number

---
+++ Data structures and Algorithms/Data-strucutures-and-algorithms/Stacks/NextGreaterElement.cs
@@ -0,0 +1,42 @@
+﻿using SharedProject.Extension;
+
+namespace Data_strucutures_and_algorithms.Stacks
+{
+    public partial class Stacks
+    {
+        //https://leetcode.com/problems/next-greater-element-i/
+        public static int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            // Map to store next greater element for each number in nums2
+            Dictionary<int, int> nextGreaterMap = [];
+            Stack<int> stack = new();
+
+            // Traverse nums2 in reverse to build the next greater map
+            for (int i = nums2.Length - 1; i >= 0; i--)
+            {
+                int current = nums2[i];
+
+                // Maintain a decreasing stack
+                while (stack.Count > 0 && stack.Peek() <= current)
+                {
+                    stack.Pop();
+                }
+
+                // If stack is not empty, the top is the next greater
+                nextGreaterMap[current] = stack.Count > 0 ? stack.Peek() : -1;
+
+                stack.Push(current);
+            }
+
+            // Construct result for nums1 using the map
+            int[] result = new int[nums1.Length];
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                result[i] = nextGreaterMap[nums1[i]];
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Data structures and Algorithms/Data-strucutures-and-algorithms/Stacks/ValidParentheses.cs
@@ -0,0 +1,50 @@
+﻿using SharedProject.Extension;
+using System.Collections.Generic;
+
+namespace Data_strucutures_and_algorithms.Stacks
+{
+    public partial class Stacks
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (IsOpenBracket(c))
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.IsEmpty())
+                    {
+                        return false;
+                    }
+                    else if(IsValidClosing(c, stack))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.IsEmpty();
+        }
+
+        private bool IsValidClosing(char c, Stack<char> stack)
+        {
+            return (c == ')' && stack.Peek() == '(') ||
+            (c == '}' && stack.Peek() == '{') ||
+            (c == ']' && stack.Peek() == '[');
+        }
+
+        private static bool IsOpenBracket(char c)
+        {
+            return c is '[' or '{' or '(';
+        }
+    }
+}
