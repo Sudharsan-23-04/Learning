2a2c807d501e7fddb18d8e89477aae1e77f87acd
Sudharsan <sudharsanbalasubramanian1@gmail.com>
2025-07-12 20:43:35 +0530

    Pushing Basic calculator

---
+++ Data structures and Algorithms/SharedProject/Extension/CharExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProject.Extension
+{
+    public static class CharExtension
+    {
+        public static int ToInt(this char c)
+        {
+            int.TryParse(c.ToString(), out var res);
+            return res;
+        }
+    }
+}
