52fde5a218cb3dcf2f90f8466a72b061c9c4ce56
Sudharsan <sudharsanbalasubramanian1@gmail.com>
2025-07-19 21:15:39 +0530

    minor changes

--- Data structures and Algorithms/Data-strucutures-and-algorithms/BinaryTree/BurnALeafNode.cs
+++ Data structures and Algorithms/Data-strucutures-and-algorithms/BinaryTree/BurnALeafNode.cs
@@ -1,6 +1,4 @@
-﻿using SharedProject.Extension;
-
-namespace Data_strucutures_and_algorithms.BinaryTree
+﻿namespace Data_strucutures_and_algorithms.BinaryTree
 {
     public partial class BinaryTreeProblem
     {
